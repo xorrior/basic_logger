0ba3425152cbc3f76cea2e6266bc792be8f77119
xorrior <xorrior@floopowder.xyz>
2025-08-27 14:40:32 -0400

    Changed logic to log events to local file

--- C2_Profiles/basic_logger/my_logger/initialize.go
+++ C2_Profiles/basic_logger/my_logger/initialize.go
@@ -7,11 +7,11 @@ import (
 func Initialize() {
 	myLoggerName := "my_logger"
 	myLogger := loggingstructs.LoggingDefinition{
-		Name:        myLoggerName,
-		Description: "basic stdout debug logger",
-		//LogToFilePath:  "mythic.log",
-		LogLevel: "debug",
-		//LogMaxSizeInMB: 20,
+		Name:           myLoggerName,
+		Description:    "basic stdout debug logger",
+		LogToFilePath:  "mythic.log",
+		LogLevel:       "debug",
+		LogMaxSizeInMB: 20,
 		//LogMaxBackups:  10,
 		NewCallbackFunction: func(input loggingstructs.NewCallbackLog) {
 			loggingstructs.AllLoggingData.Get(myLoggerName).LogInfo(input.Action, "data", input)
@@ -20,19 +20,19 @@ func Initialize() {
 			loggingstructs.AllLoggingData.Get(myLoggerName).LogInfo(input.Action, "data", input.Data)
 		},
 		NewPayloadFunction: func(input loggingstructs.NewPayloadLog) {
-			loggingstructs.AllLoggingData.Get(myLoggerName).LogInfo(input.Action, "data", input.Data)
+			//loggingstructs.AllLoggingData.Get(myLoggerName).LogInfo(input.Action, "data", input.Data)
 		},
 		NewKeylogFunction: func(input loggingstructs.NewKeylogLog) {
-			loggingstructs.AllLoggingData.Get(myLoggerName).LogInfo(input.Action, "data", input.Data)
+			//loggingstructs.AllLoggingData.Get(myLoggerName).LogInfo(input.Action, "data", input.Data)
 		},
 		NewCredentialFunction: func(input loggingstructs.NewCredentialLog) {
-			loggingstructs.AllLoggingData.Get(myLoggerName).LogInfo(input.Action, "data", input.Data)
+			//loggingstructs.AllLoggingData.Get(myLoggerName).LogInfo(input.Action, "data", input.Data)
 		},
 		NewArtifactFunction: func(input loggingstructs.NewArtifactLog) {
-			loggingstructs.AllLoggingData.Get(myLoggerName).LogInfo(input.Action, "data", input.Data)
+			//loggingstructs.AllLoggingData.Get(myLoggerName).LogInfo(input.Action, "data", input.Data)
 		},
 		NewFileFunction: func(input loggingstructs.NewFileLog) {
-			loggingstructs.AllLoggingData.Get(myLoggerName).LogInfo(input.Action, "data", input.Data)
+			//loggingstructs.AllLoggingData.Get(myLoggerName).LogInfo(input.Action, "data", input.Data)
 		},
 		NewResponseFunction: func(input loggingstructs.NewResponseLog) {
 			loggingstructs.AllLoggingData.Get(myLoggerName).LogInfo(input.Action, "data", input.Data)
