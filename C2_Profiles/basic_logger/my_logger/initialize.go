4b8f92a32ff9a1486e06cb392cd91a265c1635b1
Chris Ross <chris@xorrior.com>
2026-01-04 08:58:27 -0500

    Uncomment logging statements in initialize.go

--- C2_Profiles/basic_logger/my_logger/initialize.go
+++ C2_Profiles/basic_logger/my_logger/initialize.go
@@ -20,7 +20,7 @@ func Initialize() {
 			loggingstructs.AllLoggingData.Get(myLoggerName).LogInfo(input.Action, "data", input.Data)
 		},
 		NewPayloadFunction: func(input loggingstructs.NewPayloadLog) {
-			//loggingstructs.AllLoggingData.Get(myLoggerName).LogInfo(input.Action, "data", input.Data)
+			loggingstructs.AllLoggingData.Get(myLoggerName).LogInfo(input.Action, "data", input.Data)
 		},
 		NewKeylogFunction: func(input loggingstructs.NewKeylogLog) {
 			//loggingstructs.AllLoggingData.Get(myLoggerName).LogInfo(input.Action, "data", input.Data)
@@ -29,10 +29,10 @@ func Initialize() {
 			//loggingstructs.AllLoggingData.Get(myLoggerName).LogInfo(input.Action, "data", input.Data)
 		},
 		NewArtifactFunction: func(input loggingstructs.NewArtifactLog) {
-			//loggingstructs.AllLoggingData.Get(myLoggerName).LogInfo(input.Action, "data", input.Data)
+			loggingstructs.AllLoggingData.Get(myLoggerName).LogInfo(input.Action, "data", input.Data)
 		},
 		NewFileFunction: func(input loggingstructs.NewFileLog) {
-			//loggingstructs.AllLoggingData.Get(myLoggerName).LogInfo(input.Action, "data", input.Data)
+			loggingstructs.AllLoggingData.Get(myLoggerName).LogInfo(input.Action, "data", input.Data)
 		},
 		NewResponseFunction: func(input loggingstructs.NewResponseLog) {
 			loggingstructs.AllLoggingData.Get(myLoggerName).LogInfo(input.Action, "data", input.Data)
